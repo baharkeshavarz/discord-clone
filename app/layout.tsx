da01cc7c29eaf00593c9309bff93908e68b9122d
BaharKeshavarz <bahar.keshavarzc@gmail.com>
2023-09-07 20:00:49 +0300

    connect prisma to mysql locally

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import type { Metadata } from 'next'
 import { Open_Sans } from 'next/font/google'
 import { ClerkProvider } from '@clerk/nextjs'
 import { ThemeProvider } from '@/components/providers/theme-provider'
+import { cn } from '@/lib/utils'
 
 const font = Open_Sans({ subsets: ['latin'] })
 
@@ -25,7 +26,7 @@ export default function RootLayout({
                  enableSystem
                  storageKey="discord-theme"
               >
-               <body className={font.className}>
+               <body className={cn(font.className, "bg-white dark:bg-[#313338]")}>
                 {children}
              </body>
             </ThemeProvider>
